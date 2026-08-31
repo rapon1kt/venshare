14805a38f7c04b246653206324ab88bdd9a491df
rapon1kt <shinnatensei@gmail.com>
2026-08-31 16:23:47 -0300

    feat: initializing project with next

---
+++ src/app/layout.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Venshare",
+  description: "A platform dedicated to connecting its users seamlessly.",
+};
+
+export default function RootLayout({ children }: LayoutProps<"/">) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}

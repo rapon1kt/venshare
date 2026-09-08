fd87bcb2eb7e3e243d449593da3eabfe9d455582
rapon1kt <shinnatensei@gmail.com>
2026-09-08 12:47:15 -0300

    feat: creating user model

---
+++ src/models/user.ts
@@ -0,0 +1,44 @@
+import mongoose, { Schema, Model } from "mongoose";
+
+export interface IUser {
+  name: string;
+  email: string;
+  image?: string;
+
+  password?: string;
+
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+    password: {
+      type: String,
+      select: false,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const UserModel: Model<IUser> =
+  mongoose.models.User || mongoose.model<IUser>("User", userSchema);

3b8b196d6dd677b6fa731155ed3f1239f77313fd
rapon1kt <shinnatensei@gmail.com>
2026-09-11 15:11:54 -0300

    refactor: changing findUserByEmail in two separated methods

--- src/repositories/user-repository.ts
+++ src/repositories/user-repository.ts
@@ -3,5 +3,10 @@ import { UserModel } from "@/models/user";
 
 export async function findUserByEmail(email: string) {
   await dbConnect();
+  return UserModel.findOne({ email }).lean();
+}
+
+export async function findUserByEmailWithPassword(email: string) {
+  await dbConnect();
   return UserModel.findOne({ email }).select("+password").lean();
 }

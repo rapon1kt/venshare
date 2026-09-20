6fa3539b8c9a9bcacb9832cb80953d9919ca3a8c
rapon1kt <shinnatensei@gmail.com>
2026-09-20 18:12:11 -0300

    feat: indexing new schemas

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,2 +1,4 @@
 export { signInSchema } from "./sign-in-schema";
 export { signUpSchema } from "./sign-up-schema";
+export { resetPasswordSchema } from "./reset-password-schema";
+export { forgotPasswordSchema } from "./forgot-password-schema";

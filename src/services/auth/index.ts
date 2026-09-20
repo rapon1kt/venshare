f8fe413926787227d61a1b0d9f9776dd1c025e60
rapon1kt <shinnatensei@gmail.com>
2026-09-20 18:10:49 -0300

    feat: indexing new services

--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -1,2 +1,4 @@
-export { authenticateUser } from "./authenticate-user";
 export { signUpUser } from "./sign-up-user";
+export { authenticateUser } from "./authenticate-user";
+export { updateUserPasswordService } from "./update-user-password";
+export { generatePasswordResetToken } from "./generate-password-reset-token";

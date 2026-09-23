b4dd08f8b98cc6c392cfccebc84e9d2748af8c6c
rapon1kt <shinnatensei@gmail.com>
2026-09-23 14:26:40 -0300

    refactor: removing unecessary user repository method

--- src/services/auth/update-user-password.ts
+++ src/services/auth/update-user-password.ts
@@ -1,21 +1,11 @@
 import { hashPassword } from "@/lib/password/password";
-import {
-  findUserById,
-  updatePasswordResetToken,
-  updateUserPassword,
-} from "@/repositories";
+import { updatePasswordResetToken, updateUserPassword } from "@/repositories";
 
 export async function updateUserPasswordService(
   userId: string,
   newPassword: string,
   resetTokenId: string,
 ) {
-  const user = await findUserById(userId);
-
-  if (!user) {
-    throw new Error("USER_NOT_FOUND");
-  }
-
   const hashedPassword = await hashPassword(newPassword);
 
   await updateUserPassword({
